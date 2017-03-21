f0b55228cbcfcfe9215983c7f488af1dee1b8bf7
think <604844655@qq.com>
2017-03-21 16:32:04 +0800

    free

---
+++ freechat/src/main/java/com/fc/init/FCont.java
@@ -0,0 +1,11 @@
+package com.fc.init;
+
+/**
+ * Created by biezhi on 2017/3/18.
+ */
+public class FCont {
+
+    public static String CHAR_URL;
+    public static String VERSION;
+    public static String ADM_PWD;
+}
